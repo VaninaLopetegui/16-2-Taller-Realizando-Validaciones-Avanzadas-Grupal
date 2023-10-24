4f5e7b1d4a00b3691d5f107cd6b3484b33fbfd73
VaninaLopetegui <vaninalopetegui@gmail.com>
2023-10-23 21:32:28 -0300

    Se implementó validación en todo el formulario con js, se checkeo ambas contraseñas, espacios vacíos y se da aviso si el modal no está checkeado

--- js/validacion.js
+++ js/validacion.js
@@ -1,27 +1,66 @@
-// Esto es una función anónima que se ejecuta sola, no precisa que la llames.
-(function () {
-    let form = document.getElementById("form")
-    form.addEventListener('submit', function (event) {
-        // Validar el checkbox de términos y condiciones
-        let terminosCheckbox = document.getElementById("terminos");
-        if (!form.checkValidity() || !terminosCheckbox.checked) {
-        event.preventDefault()
-        event.stopPropagation()
-        }
-        // Si todo sale bien, le agrega al form la clase was-validated y se envía todo correctamente
-        form.classList.add('was-validated')
-    }, false)
-})()
+let form = document.getElementById("form");
 
+let terminosCheckbox = document.getElementById("terminos");
+let termsBtn = document.getElementById("termsBtn");
+let termsLabel = document.getElementById("termsLabel");
+let p1 = document.getElementById("password1");
+let p2 = document.getElementById("password2");
 
-// function revisarLargoContraseña(){
-    
-// }
+function checkPasswords(p1, p2){
+    return p1==p2;
+}
 
-// function revisarContraseñasIguales(){
-    
-// }
+function isValid(input){
+    input.classList.add("is-valid");
+    input.classList.remove("is-invalid");
+    input.style.color="green";
+}
 
-// function revisarContraseñas(){
-// }
+function isInvalid(input){
+    input.classList.add("is-invalid");
+    input.classList.remove("is-valid");
+    input.style.color="red";
+}
+
+function validate(){
+    // Validar el checkbox de términos y condiciones
+
+    if (!form.checkValidity() || !terminosCheckbox.checked || !checkPasswords(p1.value,p2.value)){
+        event.preventDefault();
+        event.stopPropagation();
+    }
+
+    if(!checkPasswords(p1.value,p2.value) || !p1.value){
+        isInvalid(p2);
+    } else {
+        isValid(p2);
+    }
+
+    if(!terminosCheckbox.checked){
+        isInvalid(termsBtn);
+        termsLabel.style.color="red";
+    } else {
+        isValid(termsBtn);
+        termsLabel.style.color="green";
+    }
+
+    // Si todo sale bien, le agrega al form la clase was-validated y se envía todo correctamente
+    form.classList.add('was-validated');
+}
+
+form.addEventListener('submit', function (event){
+    validate();
+
+    p1.addEventListener("input", function(){
+        validate();
+    });
+
+    p2.addEventListener("input", function(){
+        validate();
+    });
+
+    terminosCheckbox.addEventListener("input", function(){
+        validate();
+    });
+});
 
